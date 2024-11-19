9ab2877566b2e5412af6acd6f11b4b74ed6a099c
vishal kumar <vishakumar041200@gmail.com>
2024-11-19 16:46:19 +0530

    navbar changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctor from './pages/Doctor'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppoinment from './pages/MyAppoinment'
+import Appointment from './pages/Appointment'
+import Navbar from './component/Navbar'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Doctor Appoinment 
-    </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctor/>}/>
+        <Route path='/doctors/:speciality' element={<Doctor/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-Appointment' element={<MyAppoinment/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
+    </div>
   )
 }
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+const y=assets
+
+  return (
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+     <img src={assets.logo} alt="" />
+     <ul>
+        <NavLink>
+          <li>Home</li><hr />
+        </NavLink>
+        <NavLink>
+          <li>All Doctors</li><hr />
+        </NavLink>
+        <NavLink>
+          <li>About</li><hr />
+        </NavLink>
+        <NavLink>
+          <li>Contact</li><hr />
+        </NavLink>
+     </ul>
+     <div>
+        <button>Create account</button>
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
